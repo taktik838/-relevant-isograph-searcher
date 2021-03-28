5ffe3191d6470323a0de64d10050ed9c6a335f4c
Олег Поперняк <popernyakoa@gmail.com>
2021-03-28 15:42:45 +0200

    done google client

---
+++ src/integrations/google/client.py
@@ -0,0 +1,40 @@
+from google.cloud import speech
+
+
+AUTH_PATH = 'src/integrations/google/secret.json'
+client = speech.SpeechAsyncClient.from_service_account_file(AUTH_PATH)
+
+
+async def speech2text(
+        content: bytes, language_code: str = 'ru-RU', channels: int = 1, rate: int = 16000, encoding: str = 'LINEAR16'
+    ) -> str:
+    audio = speech.RecognitionAudio(content=content)
+    try:
+        encoding = speech.RecognitionConfig.AudioEncoding[encoding.upper()]
+    except KeyError:
+        raise
+    
+    config = speech.RecognitionConfig(
+        encoding=encoding,
+        sample_rate_hertz=rate,
+        language_code=language_code,
+        audio_channel_count=channels,
+        
+    )
+
+    response = await client.recognize(config=config, audio=audio)
+
+    
+    return response.results[0].alternatives[0].transcript
+
+
+if __name__ == '__main__':
+    import asyncio
+    
+    async def main():
+        path = 'src/integrations/google/ru16.flac'
+        content = open(path, 'rb').read()
+        print(await speech2text(content, encoding='flac', language_code='ru'))
+    
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
