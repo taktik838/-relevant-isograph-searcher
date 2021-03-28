e05441a26691e483c5cb1fc2ef01574f09c5884a
Олег Поперняк <popernyakoa@gmail.com>
2021-03-28 16:12:19 +0200

    hide getAll

--- src/transport/routes.py
+++ src/transport/routes.py
@@ -5,7 +5,7 @@ from transport.handlers import searcher
 
 def setup_routes(app: web.Application):
     app.router.add_get(r'/api/entity/get/byText/{page:\d?}', searcher.byText, allow_head=False)
-    app.router.add_get(r'/api/entity/get/all/{page:\d?}', searcher.get_all, allow_head=False)
+    # app.router.add_get(r'/api/entity/get/all/{page:\d?}', searcher.get_all, allow_head=False)
     app.router.add_get('/api/entity/get/byUrl', searcher.byUrl, allow_head=False)
     app.router.add_post('/api/entity/get/bySpeech', searcher.bySpeech)
     
